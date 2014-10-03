88437d21fed8e611f756ff2a1b75fe085b16a6ab
mor <kadoshms@gmail.com>
2014-10-04 02:37:03 +0300

    Integrated with parse

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Supercard web service
+ * Uses parse RESTful API
+ */
+// the class
+var Kaiseki = require('kaiseki');
+// instantiate
+var APP_ID = 'CtKrDhLEMo8k05hV4HekNMkx4MXmpBercuzruQ2T';
+var REST_API_KEY = 'wOcrKgh0Q6icbGJ4gotHtmLpYdVPntPhhMKpGVsc';
+var kaiseki = new Kaiseki(APP_ID, REST_API_KEY);
