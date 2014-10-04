27aea2e1ad78e426c66ad699e4072b8b4a98eb6e
mor <kadoshms@gmail.com>
2014-10-04 13:34:42 +0300

    Added restricions routes to API

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ var bodyParser = require('body-parser');
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var purchases = require('./routes/purchases'); //routes are defined here
+//API routes
+var purchases = require('./routes/purchases');
+var restrictions = require('./routes/restrictions');
 var app = express(); //Create the Express app
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use('/api', purchases); //This is our route middleware
+//API declares
+app.use('/api', purchases);
+app.use('/api', restrictions);
 app.listen();
 module.exports = app;
