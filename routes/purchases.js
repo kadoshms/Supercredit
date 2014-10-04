a222fdf6bc74321f351e92814c85644f3cc65405
mor <kadoshms@gmail.com>
2014-10-04 11:26:25 +0300

    Purchases are now a module

--- routes/purchases.js
+++ routes/purchases.js
@@ -1,10 +1,6 @@
 var express = require('express');
-var Kaiseki = require('kaiseki');
+var Purch = require("purchase");
 var router = express.Router();
-//instantiate
-var APP_ID = 'CtKrDhLEMo8k05hV4HekNMkx4MXmpBercuzruQ2T';
-var REST_API_KEY = 'wOcrKgh0Q6icbGJ4gotHtmLpYdVPntPhhMKpGVsc';
-var kaiseki = new Kaiseki(APP_ID, REST_API_KEY);
 /**
  * Save a new purchase to parse
  * method: POST
@@ -12,16 +8,7 @@ var kaiseki = new Kaiseki(APP_ID, REST_API_KEY);
  */
 router.route('/purchases')
 .post(function(req, res) {
-	var className = 'Purchases';
-	var purchase = [{
-			'userId' : req.param('userId'),
-			'purchase_type' : req.param('purchase_type'),
-			'purchase_amount' : req.param('purchase_amount')
-	}]
-	kaiseki.createObjects(className, purchase, function(err, res, body, success) {
-		console.log(err);
-	            console.log('objects created = ', body);
-	});
+	Purch.newPurchase(req)
 	res.send({ message: 'added' });
 });
 module.exports=router;
